e860d3ddbf9536d98317ad76f5a9d4bcaa6af631
xuegenjin <xuegen.jin@gmail.com>
2023-12-28 23:21:42 +0000

    member function

---
+++ member.js
@@ -0,0 +1,20 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var project = document.getElementById("project");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var skillsMember = document.getElementById("skillsMember");
+    var projectMember = document.getElementById("projectMember");
+    var contactMember = document.getElementById("contactMember");
+    var aboutMember = document.getElementById("aboutMember");
+    member.style.display = "block";
+    skills.style.display = "none";
+    project.style.display = "none";
+    contact.style.display = "none";
+    about.style.display = "none";
+    skillsMember.style.display = "block";
+    projectMember.style.display = "none";
+    contactMember.style.display = "none";
+    aboutMember.style.display = "none";
+}
