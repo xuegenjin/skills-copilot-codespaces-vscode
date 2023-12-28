f251cf86f0f234de00e504bebf70fea98bc4e77c
xuegenjin <xuegen.jin@gmail.com>
2023-12-28 23:41:05 +0000

    refactoring the web server code

--- comments.js
+++ comments.js
@@ -1,13 +1,17 @@
-//create a simple web server that responds with "Hello World" to all requests, and is listening on port 8888
+//create a simple web server that responds with "Hello World" to all requests, 
+//and is listening on port 9999
+// http://localhost:9999
+
 const http = require('http');
-const port = 8888;
+const port = 9999;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
+    res.setHeader('Content-Type', 'text/plain'); //text/html
     res.end('Hello World');
 });
 
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
+
